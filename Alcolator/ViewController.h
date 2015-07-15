a1cb7f23872fe1afd587386af8e1b25a5c48886b
Roger Hom <roger.c.hom@gmail.com>
2015-07-15 08:33:31 -0700

    Initialize xcode project

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by psytronx on 7/15/15.
+//  Copyright (c) 2015 Logical Dimension. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
