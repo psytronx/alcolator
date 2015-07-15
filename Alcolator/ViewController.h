f5656bdc275ea142369ee24d7906bef3effb0c92
Roger Hom <roger.c.hom@gmail.com>
2015-07-15 14:30:12 -0700

    Move properties of ViewController from .m to .h file

--- Alcolator/ViewController.h
+++ Alcolator/ViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
+@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 
 
 @end
