14dc288fa8bc1db051f8d76aa16d10bed5d5bf76
Roger Hom <roger.c.hom@gmail.com>
2015-07-16 14:00:21 -0700

    Add navigation bar and menu view controller. Make segues for buttons in menu to transition to wine and whiskey view controllers

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by psytronx on 7/16/15.
+//  Copyright (c) 2015 Logical Dimension. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
