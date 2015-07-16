714c251e2855a4d31126b895cb62f5b476fcc95d
Roger Hom <roger.c.hom@gmail.com>
2015-07-16 10:22:30 -0700

    Add WhiskeyViewController

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by psytronx on 7/15/15.
+//  Copyright (c) 2015 Logical Dimension. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
