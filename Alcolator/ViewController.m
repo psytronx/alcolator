fd5bce8a10f9468eb2aa694fe93512ad193b2d42
Roger Hom <roger.c.hom@gmail.com>
2015-07-17 12:51:01 -0700

    Switch code to use Tab View Controller

--- Alcolator/ViewController.m
+++ Alcolator/ViewController.m
@@ -35,10 +35,11 @@
 
 
 - (IBAction)sliderValueDidChange:(UISlider *)sender {
-    
     NSLog(@"Slider value changed to %f", sender.value);
     [self.beerPercentTextField resignFirstResponder];
+    [self.tabBarItem setBadgeValue:[NSString stringWithFormat:@"%d", (int) sender.value]];
 }
+
 
 - (IBAction)buttonPressed:(UIButton *)sender {
     
